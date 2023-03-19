38dcd4c7dcbe830c284b69f524fabb661cde9fe2
FallenChromium <fallenchromium@gmail.com>
2023-03-19 17:00:54 +0300

    Rename file, add path prefix

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 TILDA_PUBLIC_KEY = os.environ.get('TILDA_PUBLIC_KEY')
 TILDA_SECRET_KEY = os.environ.get('TILDA_SECRET_KEY')
+LOCAL_PATH_PREFIX = os.environ.get('LOCAL_PATH_PREFIX', '')
 
 @app.route('/webhook', methods=['POST'])
 def handle_webhook():
@@ -16,7 +17,7 @@ def handle_webhook():
     project_info = requests.get(f'https://api.tilda.cc/v1/projects/getprojectinfo/?projectid={payload["projectid"]}&publickey={TILDA_PUBLIC_KEY}&secretkey={TILDA_SECRET_KEY}')
     for image in project_info.json()['images']:
         source_url = image['from']
-        local_path = image['to']
+        local_path = LOCAL_PATH_PREFIX + image['to']
         save_image(source_url, local_path)
 
     # Send the getpageslist request and loop through the page list to export each page
@@ -26,25 +27,25 @@ def handle_webhook():
         page_info = requests.get(f'https://api.tilda.cc/v1/pages/getpagefullexport/?pageid={page["id"]}&projectid={payload["projectid"]}&publickey={TILDA_PUBLIC_KEY}&secretkey={TILDA_SECRET_KEY}')
         for image in page_info.json()['images']:
             source_url = image['from']
-            local_path = image['to']
+            local_path = LOCAL_PATH_PREFIX + image['to']
             save_image(source_url, local_path)
 
         for script in page_info.json()['js']:
             source_url = script['from']
-            local_path = script['to']
+            local_path = LOCAL_PATH_PREFIX + script['to']
             save_script(source_url, local_path)
 
         for style in page_info.json()['css']:
             source_url = style['from']
-            local_path = style['to']
+            local_path = LOCAL_PATH_PREFIX + style['to']
             save_style(source_url, local_path)
 
         # Create a new page file and fill it with the HTML content
         filename = page_info.json()['filename']
         html_content = page_info.json()['html']
         create_page_file(filename, html_content)
-    # return 'ok' as per Tilda API docs
-    return 'ok'
+
+    return 'OK'
 
 def save_image(source_url, local_path):
     # Download the image from the source URL and save it to the local path
